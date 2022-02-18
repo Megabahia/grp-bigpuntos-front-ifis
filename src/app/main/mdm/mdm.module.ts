4609ea7553c550f8eab50cf93cd5431a526c5d88
Abraham Vallejos <abraham-rat@outlook.es>
2022-02-18 15:38:51 -0500

    vistas MDO

--- src/app/main/mdm/mdm.module.ts
+++ src/app/main/mdm/mdm.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AuthGuard } from '../../auth/helpers/auth.guards';
-import { CoreCommonModule } from '../../../@core/common.module';
-import { RouterModule } from '@angular/router';
-import { Role } from '../../auth/models/role';
-import { ContentHeaderModule } from '../../layout/components/content-header/content-header.module';
-import { TranslateModule } from '@ngx-translate/core';
-import { SwiperModule } from 'ngx-swiper-wrapper';
-import { CoreTouchspinModule } from '../../../@core/components/core-touchspin/core-touchspin.module';
-import { CoreSidebarModule } from '../../../@core/components/core-sidebar/core-sidebar.module';
-import { Ng2FlatpickrModule } from 'ng2-flatpickr';
-import { CardSnippetModule } from '../../../@core/components/card-snippet/card-snippet.module';
-import { ShareIconsModule } from 'ngx-sharebuttons/icons';
-import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AuthGuard } from "../../auth/helpers/auth.guards";
+import { CoreCommonModule } from "../../../@core/common.module";
+import { RouterModule } from "@angular/router";
+import { Role } from "../../auth/models/role";
+import { ContentHeaderModule } from "../../layout/components/content-header/content-header.module";
+import { TranslateModule } from "@ngx-translate/core";
+import { SwiperModule } from "ngx-swiper-wrapper";
+import { CoreTouchspinModule } from "../../../@core/components/core-touchspin/core-touchspin.module";
+import { CoreSidebarModule } from "../../../@core/components/core-sidebar/core-sidebar.module";
+import { Ng2FlatpickrModule } from "ng2-flatpickr";
+import { CardSnippetModule } from "../../../@core/components/card-snippet/card-snippet.module";
+import { ShareIconsModule } from "ngx-sharebuttons/icons";
+import { ShareButtonsModule } from "ngx-sharebuttons/buttons";
 // import { ParamsComponent } from './params/params.component';
 // import { SharedModule } from 'src/app/shared/shared.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 // import { PersonasListComponent } from './clientes/personas/personas-list/personas-list.component';
 // import { PersonasEditComponent } from './clientes/personas/personas-edit/personas-edit.component';
 // import { TransaccionesListComponent } from './clientes/personas/transacciones-list/transacciones-list.component';
@@ -29,29 +29,35 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { NegociosListComponent } from './clientes/negocios/negocios-list/negocios-list.component';
 // import { NegociosEditComponent } from './clientes/negocios/negocios-edit/negocios-edit.component';
 // import { NegociosLoadComponent } from './clientes/negocios/negocios-load/negocios-load.component';
-import { ProspectosClientesListComponent } from './prospectos-clientes/prospectos-clientes-list/prospectos-clientes-list.component';
+import { ProspectosClientesListComponent } from "./prospectos-clientes/prospectos-clientes-list/prospectos-clientes-list.component";
 // import { ProspectosClientesAddCsvComponent } from './prospectos-clientes/prospectos-clientes-add-csv/prospectos-clientes-add-csv.component';
 // import { ProspectosClientesAddXlsxComponent } from './prospectos-clientes/prospectos-clientes-add-xlsx/prospectos-clientes-add-xlsx.component';
 // import { ProspectosClientesEditComponent } from './prospectos-clientes/prospectos-clientes-edit/prospectos-clientes-edit.component';
-import { ChartsModule } from 'ng2-charts';
-import {FlatpickrModule} from 'angularx-flatpickr';
+import { ChartsModule } from "ng2-charts";
+import { FlatpickrModule } from "angularx-flatpickr";
+import { PersonasListComponent } from "./clientes/personas/personas-list/personas-list.component";
 // import { PersonasParientesComponent } from './clientes/personas/personas-parientes/personas-parientes.component';
 
 const routes = [
-  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: "", redirectTo: "list", pathMatch: "full" },
   {
-    path: 'list',
+    path: "list",
     component: ProspectosClientesListComponent,
     data: { roles: [Role.SuperMonedas] },
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    // data: { animation: 'auth' }
+  },
+  {
+    path: "listClientes",
+    component: PersonasListComponent,
+    data: { roles: [Role.SuperMonedas] },
+    canActivate: [AuthGuard],
     // data: { animation: 'auth' }
   },
 ];
 
 @NgModule({
-  declarations: [
-    ProspectosClientesListComponent,
-  ],
+  declarations: [ProspectosClientesListComponent, PersonasListComponent],
   imports: [
     CoreCommonModule,
     RouterModule.forChild(routes),
@@ -65,10 +71,8 @@ const routes = [
     Ng2FlatpickrModule,
     CardSnippetModule,
     ShareIconsModule,
-    ShareButtonsModule
+    ShareButtonsModule,
   ],
-  exports: [
-    ProspectosClientesListComponent,
-  ]
+  exports: [ProspectosClientesListComponent, PersonasListComponent],
 })
-export class MdmModule { }
+export class MdmModule {}
